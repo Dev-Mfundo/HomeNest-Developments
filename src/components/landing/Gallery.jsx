4f9198bc091b9c8486b4ce0206669fb2e157f8f5
devmfundo <devmfundo@gmail.com>
2024-09-06 01:47:47 +0200

    saving progress

--- src/components/landing/Gallery.jsx
+++ src/components/landing/Gallery.jsx
@@ -4,12 +4,12 @@ function Gallery(){
         <div className="developments-gallery">
             <h3>Housing Developments</h3>
             <div className="gallery-grid">
-                <div className="gallery-item"><img src="fourth.PNG" alt=""/></div>
-                <div className="gallery-item"><img src="secondOne.PNG" alt=""/></div>
-                <div className="gallery-item"><img src="thirdOne.PNG" alt=""/></div>
-                <div className="gallery-item"><img src="upperOne.PNG" alt=""/></div>
+                <div className="gallery-item"><img src="/fourth.PNG" alt=""/></div>
+                <div className="gallery-item"><img src="/secondOne.PNG" alt=""/></div>
+                <div className="gallery-item"><img src="/thirdOne.PNG" alt=""/></div>
+                <div className="gallery-item"><img src="/upperOne.PNG" alt=""/></div>
                 <div className="gallery-item"><img src="/upperTwo.PNG" alt=""/></div>
-                <div className="gallery-item"><img src="/anotherOne" alt="images"/></div>
+                <div className="gallery-item"><img src="/anotherOne.PNG" alt=""/></div>
             </div>
         </div>
     </div>
